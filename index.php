ea4d4c3d31bfb3509749e924afd113ced3b39125
futoase <futoase@gmail.com>
2014-02-05 17:49:12 +0900

    Modified: Load widget.js

--- index.php
+++ index.php
@@ -76,5 +76,6 @@
       <div id="map_canvas" ></div>
   </div>
 <div id="load_lock"><img src="js/loading.gif" />検索中</div>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 </body>
 </html>
